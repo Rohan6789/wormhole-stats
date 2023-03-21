53ff84f9389ac11b60ba17b663529da97b26ab81
Rohan Khandelwal <rohankh18@gmail.com>
2023-03-21 01:02:58 -0700

    react app template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+  // api calls
+
+  const router = require('axios');
+  const response = getEthScan(axios).then(res => console.log(res))
+
+
+  // var api = require('etherscan-api').init('YQMRKMD93AIZUWZVJ8YXWXWJV65PGG8J3F');
+  // var balance = api.account.balance('0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae');
+  // balance.then(function(balanceData){
+  //   console.log(balanceData);
+  // });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      asdasd
     </div>
   );
+}
+
+export async function getEthScan(router) {
+  const addr = "0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae"
+  const apikey = "YQMRKMD93AIZUWZVJ8YXWXWJV65PGG8J3F"
+  return await router(`https://api.etherscan.io/api?module=account&action=balance&address=${addr}&tag=latest&apikey=${apikey}`)
 }
 
 export default App;
